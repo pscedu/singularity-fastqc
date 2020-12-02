d47e2cc04d85fcd6373f27870ffea1a944c6e1a6
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2020-12-01 21:31:45 -0500

    Updated repo for Bridges2

---
+++ 0.11.9/modulefile.lua
@@ -0,0 +1,51 @@
+--
+-- FastQC 0.11.9 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Biological Sciences"
+-- "Description: FastQC aims to provide a simple way to do some quality control checks on raw sequence data coming from high throughput sequencing pipelines."
+-- "Keywords: singularity bioinformatics"
+
+whatis("Name: fastqc")
+whatis("Version: 0.11.9")
+whatis("Category: Biological Sciences")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Description: FastQC aims to provide a simple way to do some quality control checks on raw sequence data coming from high throughput sequencing pipelines.")
+
+help([[
+fastqc 0.11.9
+--------------
+
+Description
+-----------
+FastQC aims to provide a simple way to do some quality control checks on raw sequence data coming from high throughput sequencing pipelines.
+
+To load the module, type
+
+> module load fastqc/0.11.9
+
+To unload the module, type
+
+> module unload fastqc/0.11.9
+
+Documentation
+-------------
+https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
+
+For help, type
+
+> fastqc --help
+
+Repository
+----------
+https://github.com/s-andrews/FastQC
+
+Tools included in this module are
+
+* fastqc
+]])
+
+local package = "FastQC"
+local version = "0.11.9"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
