98dd6577f2096e7a5f2122f3040205193842162b
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2021-08-13 00:24:33 -0400

    Updated Singularity definition file

--- 0.11.9/pull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if ! [ -x "$(command -v singularity)" ]; then
-	module load singularity
-fi
-
-singularity pull https://depot.galaxyproject.org/singularity/fastqc%3A0.11.9--0
-mv fastqc%3A0.11.9--0 singularity-FastQC-0.11.9.sif
